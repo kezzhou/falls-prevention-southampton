e801e93856e04749030c23f0f5f64812b533009c
kezzhou <102555416+kezzhou@users.noreply.github.com>
2022-11-24 22:49:45 -0500

    Update 10
    added upload file

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -11,21 +11,30 @@ st.title('Southampton Falls Prevention 2022')
 
 #### Dashboard Header and Description ####
 
-
-df = pd.read_csv('../data/patients.csv')
-
-df = df[df['age'] > 65]
-
-df = df[df['er_disposition'].isin(['fasttrac', 'home'])]
-
 st.header('Description')
 
 st.caption('Southampton Hospital Fall Prevention Sector has developed a dashboard complete with monthly database updates, patient and EBP program geo tracking, Tableu compatibility, and patient information statistical analysis. The focus demographic of our program is patients 65 and over with a primary or secondary diagnosis of a fall. Once these patients are discharged, our program tracks and contacts them for follow-up debriefing and enrollment in relevant EBP programs based on proximity and relevancy. Only patients who have granted consent and signed the appropriate forms prior to discharge have been contacted.')
+
+#### Upload your own dataset ####
+
+upload = st.file_uploader(label="Upload your file here", type='csv')
+
+
 
 ### Patient Master List #### 
 
 
 st.header('Patient List')
+
+if upload is not None:
+    df = pd.read_csv(upload)
+else:
+    df = pd.read_csv('../data/patients.csv')
+
+
+df = df[df['age'] > 65]
+
+df = df[df['er_disposition'].isin(['fasttrac', 'home'])]
 
 df = df[['acct', 'mrn', 'svc', 'stay_type', 'last_name',
        'first_name', 'middle_name', 'dob', 'age', 'gender', 'address1', 'address2',
@@ -36,6 +45,35 @@ df = df[['acct', 'mrn', 'svc', 'stay_type', 'last_name',
 
 
 st.dataframe(df)
+
+
+#### Headers ####
+st.header('Analytics')
+
+#### Barchart - Patients by Age ####
+
+df = df['age'].value_counts()
+
+st.subheader('Patients by Age')
+
+st.bar_chart(df)
+
+st.caption("A bar graph representation of patient age according to the dataset")
+
+
+
+
+### Line Chart - Fall Incidents over time ####
+
+st.subheader('Southampton Hospital Fall Admits From 2021 to 2022')
+
+df = pd.DataFrame({
+   'admits': [1, 8, 11, 3, 8, 8, 6, 8, 7, 8, 6, 9, 10, 13]
+   }, index=['2021/11','2021/12','2022/01', '2022/02','2022/03','2022/04','2022/05','2022/06','2022/07','2022/08','2022/09','2022/10','2022/11','2022/12'])
+
+st.line_chart(df)
+
+st.caption("A line chart displaying admits for falls to Southampton Hospital from 2021 to 2022")
 
 
 ### Header ###
@@ -154,33 +192,3 @@ if st.button("Submit"):
     else:
         df=df[df['ebp'] == ebpName]
         st.write(df)
-
-#### Headers ####
-st.header('Analytics')
-
-#### Barchart - Patients by Age ####
-
-df = pd.read_csv('../data/patients.csv')
-
-df = df['age'].value_counts()
-
-st.subheader('Patients by Age')
-
-st.bar_chart(df)
-
-st.caption("A bar graph representation of patient age according to the dataset")
-
-
-
-
-### Line Chart - Fall Incidents over time ####
-
-st.subheader('Southampton Hospital Fall Admits From 2021 to 2022')
-
-df = pd.DataFrame({
-   'admits': [1, 8, 11, 3, 8, 8, 6, 8, 7, 8, 6, 9, 10, 13]
-   }, index=['2021/11','2021/12','2022/01', '2022/02','2022/03','2022/04','2022/05','2022/06','2022/07','2022/08','2022/09','2022/10','2022/11','2022/12'])
-
-st.line_chart(df)
-
-st.caption("A line chart displaying admits for falls to Southampton Hospital from 2021 to 2022")
