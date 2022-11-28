6a91dda6440e011ab68e518bede60fd2d71d99cb
kezzhou <102555416+kezzhou@users.noreply.github.com>
2022-11-28 00:29:05 -0500

    Update 12
    Updated line chart functionality, added side bar for convenience when using the ebp pair module, updated requirements.txt

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import streamlit as st
 import pgeocode as pg
+import plotly_express as px
 
 #### Title ####
 
@@ -17,7 +18,7 @@ st.caption('Southampton Hospital Fall Prevention Sector has developed a dashboar
 
 #### Upload your own dataset ####
 
-upload = st.file_uploader(label="Upload your file here", type='csv')
+upload = st.file_uploader(label="Upload your file here", type='csv') ## upload your own csv that changes the analytics
 
 
 
@@ -25,6 +26,8 @@ upload = st.file_uploader(label="Upload your file here", type='csv')
 
 
 st.header('Patient List')
+
+st.caption('Uploaded patient list filtered for patients 65 years of age or older and with an ER Disposition of Home')
 
 if upload is not None:
     df = pd.read_csv(upload)
@@ -42,7 +45,7 @@ df = df[['acct', 'mrn', 'svc', 'stay_type', 'last_name',
        'discharge', 'er_disposition', 'final_disch_disposition',
        'final_disch_disp_desc', 'pcp_number', 'pcp_name',
        'er_log_chief_complaint', 'cpsi_chief_complaint']]
-
+## we do this manual filtering for aesthetics - to exclude the default id columns and whatnot
 
 st.dataframe(df)
 
@@ -50,15 +53,18 @@ st.dataframe(df)
 #### Headers ####
 st.header('Analytics')
 
+st.caption('Quick statistics to represent patient data as well as in-depth Tableau analysis')
+
 #### Barchart - Patients by Age ####
 
-df = df['age'].value_counts()
+
+df1 = df['age'].value_counts()
 
 st.subheader('Patients by Age')
-
-st.bar_chart(df)
-
 st.caption("A bar graph representation of patient age according to the dataset")
+
+st.bar_chart(df1)
+
 
 
 
@@ -67,28 +73,37 @@ st.caption("A bar graph representation of patient age according to the dataset")
 
 st.subheader('Southampton Hospital Fall Admits From 2021 to 2022')
 
-df = pd.DataFrame({
-   'admits': [1, 8, 11, 3, 8, 8, 6, 8, 7, 8, 6, 9, 10, 13]
-   }, index=['2021/11','2021/12','2022/01', '2022/02','2022/03','2022/04','2022/05','2022/06','2022/07','2022/08','2022/09','2022/10','2022/11','2022/12'])
-
-st.line_chart(df)
-
 st.caption("A line chart displaying admits for falls to Southampton Hospital from 2021 to 2022")
 
+df['admit'] = pd.to_datetime(df['admit']) # we change the admit dates to datetime format to extract month and date
+
+df['monthyear'] = df['admit'].dt.to_period('M') # we extract month and date into a separate column because we want to classify it by that 
+
+df['monthyear'].sort_values() # sort the values ascending
+
+df = df['monthyear'].value_counts() # count how many cases are in each month of 2021 and 2022 (or other years present in the file)
+
+st.area_chart(df)
+
+## can't get streamlit to recognize the month-year dates as the x-axis values. this may be an issue with value_counts(), but it works fine for the barchart.
+## the same problem is present with plotly ex line charts.
 
 ### Header ###
 
 st.header('Geo Data')
+st.caption('Interactive maps with patient and EBP locations')
 
 
 
 #### Geo Data Patients ####
 
 st.subheader('Patient Geo Data')
+st.caption('A map of patient addresses to aid in pairing them with appropriate evidence-based programs and communities')
+
 
 df = pd.read_csv('../data/geo.csv')
 
-st.map(df, zoom=12)
+st.map(df, zoom=12) # we set the default zoom to focus on patients around the Southampton area as per client request
 
 df = pd.read_csv('../data/patientgeo.csv')
 
@@ -96,13 +111,14 @@ df = df[['mrn', 'lat', 'lon']]
 
 if st.checkbox('Show geo data corresponding to patients by MRN'):
     st.dataframe(df)
-
-st.caption('A map of patient addresses to aid in pairing them with appropriate evidence-based programs and communities')
+## checkbox for optionally showing coordinates
 
 
 #### EBP Geo Data ####
 
 st.subheader('Evidence-Based Programs Geo Data')
+st.caption('A map of EBPs to allow for easy pairing of discharged patients to EBP by proximity data')
+
 
 df = pd.read_csv('../data/ebpgeo.csv')
 
@@ -113,38 +129,40 @@ df = df[['ebp', 'lat', 'lon']]
 if st.checkbox('Show geo data corresponding to Evidence-Based Program'):
     st.dataframe(df)
 
-st.caption('A map of EBPs to allow for easy pairing of discharged patients to EBP by proximity data')
 
 
 ####Header ####
 
-st.header('Evidence-Based Programs')
+with st.sidebar:
+
+    st.header('Evidence-Based Programs')
 
 #### Evidence-Based Programs ####
 
-st.subheader('Current Southampton Evidence-Based Programs')
+    st.subheader('Current Southampton Evidence-Based Programs')
+    st.caption('A list of EBPs currently approved by Southampton Falls Prevention')
 
-df = pd.read_csv('../data/masterlistebp.csv')
 
-df = df[df['status'] == 'current']
+    df = pd.read_csv('../data/masterlistebp.csv')
 
-df = df[['name', 'attendance', 'links']]
+    df = df[df['status'] == 'current']
 
-st.dataframe(df)
+    df = df[['name', 'attendance', 'links']]
 
-st.caption('A list of EBPs currently approved by Southampton Falls Prevention')
+    st.dataframe(df)
 
-st.subheader('Suggested Southampton Evidence-Based Programs')
 
-df = pd.read_csv('../data/masterlistebp.csv')
+    st.subheader('Suggested Southampton Evidence-Based Programs')
+    st.caption('A list of EBPs suggested for approval by Southampton Falls Prevention')
 
-df = df[df['status'] == 'suggested']
 
-df = df[['name', 'attendance', 'links']]
+    df = pd.read_csv('../data/masterlistebp.csv')
 
-st.dataframe(df)
+    df = df[df['status'] == 'suggested']
 
-st.caption('A list of EBPs suggested for approval by Southampton Falls Prevention')
+    df = df[['name', 'attendance', 'links']]
+
+    st.dataframe(df)
 
 
 
@@ -152,6 +170,8 @@ st.caption('A list of EBPs suggested for approval by Southampton Falls Preventio
 
 # Title
 st.header("Patient-EBP Pairing System")
+st.caption('Pairing tool module that allows user to pair patients with EBPs based on inputs zipcode and optionally EBP name and max distance radius')
+
 
 # Input bar 1
 zipcode = st.number_input("Enter Zip Code", min_value=00000, max_value=99999)
@@ -167,7 +187,7 @@ if st.button("Submit"):
     ## calculate 
     def get_distance(x, y):
         usa_zipcodes = pg.GeoDistance('us')
-        distance_in_kms = usa_zipcodes.query_postal_code(x, y)
+        distance_in_kms = usa_zipcodes.query_postal_code(x, y) ## this package usefully helps calculate distance between zipcodes in km
         return distance_in_kms
 
     ## loop through dataframe and calculate distance
@@ -188,7 +208,10 @@ if st.button("Submit"):
         df=df[['ebp', 'zip', 'distance (km)']]
 
     if ebpName == '':
+        st.success('Done!')
         st.write(df)
     else:
         df=df[df['ebp'] == ebpName]
+        st.success('Done!')
         st.write(df)
+
